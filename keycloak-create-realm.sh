fbb7f5ab47959e3cf9ecb120dbcaf409800fabc3
Jithin <jithin.joseph@allex.ai>
2026-01-31 19:43:40 +0530

    updating vikunja oidc

---
+++ keycloak-create-realm.sh
@@ -0,0 +1,37 @@
+kubectl exec -n keycloak keycloak-0 -- /bin/bash -c '
+  # 1. Login (Just in case)
+  /opt/bitnami/keycloak/bin/kcadm.sh config credentials \
+    --server http://localhost:8080/auth \
+    --realm master \
+    --user admin \
+    --password Password123! \
+    --config /tmp/kcadm.config
+
+  # 2. Create "vikunja" Realm (with SSL Disabled!)
+  echo "Creating Realm..."
+  /opt/bitnami/keycloak/bin/kcadm.sh create realms \
+    -s realm=vikunja \
+    -s enabled=true \
+    -s sslRequired=NONE \
+    --config /tmp/kcadm.config
+
+  # 3. Create "vikunja-app" Client
+  echo "Creating Client..."
+  /opt/bitnami/keycloak/bin/kcadm.sh create clients -r vikunja \
+    -s clientId=vikunja-app \
+    -s protocol=openid-connect \
+    -s publicClient=false \
+    -s directAccessGrantsEnabled=true \
+    -s standardFlowEnabled=true \
+    -s "redirectUris=[\"http://35.190.33.137/*\"]" \
+    -s "webOrigins=[\"*\"]" \
+    -s secret="VikunjaSecret2026!" \
+    --config /tmp/kcadm.config
+
+  echo "----------------------------------------------"
+  echo "✅ SUCCESS!"
+  echo "Client ID:     vikunja-app"
+  echo "Client Secret: VikunjaSecret2026!"
+  echo "Realm URL:     http://35.190.33.137/auth/realms/vikunja"
+  echo "----------------------------------------------"
+'
